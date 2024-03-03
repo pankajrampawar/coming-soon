c70fc70e268fdc1587b08d748988403c9bf2936a
pankaj ram pawar <pankajpawars123@gmail.com>
2024-03-03 18:56:15 +0530

    added all files

---
+++ app/fonts.js
@@ -0,0 +1,4 @@
+import { Happy_Monkey, Keania_One } from "next/font/google";
+
+export const happyMonkey = Happy_Monkey({ subsets: ["latin"], weight: ["400"]})
+export const keaniaOne = Keania_One({ subsets: ["latin"], weight: ["400"]})
