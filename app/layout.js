de7cc7f0d880a3645ac51492aaa3e6600fecb7fa
pankaj ram pawar <pankajpawars123@gmail.com>
2024-03-04 22:18:23 +0530

    analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Happy_Monkey, Keania_One } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 const happyMonkey = Happy_Monkey({ subsets: ["latin"], weight: ['400']})
@@ -13,6 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={`${happyMonkey.className} bg-black text-white flex justify-center items-center overflow-hidden min-h-screen max-h-screen max-w-screen text-4xl sm:text-6xl`}>{children}</body>
     </html>
   );
